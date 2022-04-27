f0bf8db01d2da54e960e5a1a5275d69b79f7a28e
usama-unitydeveloper <76942661+usama-unitydeveloper@users.noreply.github.com>
2022-04-28 02:14:30 +0500

    init

---
+++ Assets/Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Controller : MonoBehaviour
+{
+    public float flySpeed = 5;
+    public float yawAmount = 120;
+    private float Yaw;
+
+    void FixedUpdate()
+    {
+
+        // move forward
+        transform.position += transform.forward * flySpeed * Time.deltaTime;
+        // inputs
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        // yaw, pitch, roll
+        Yaw += horizontalInput * yawAmount * Time.deltaTime;
+        float pitch = Mathf.Lerp(0, 20, Mathf.Abs(verticalInput)) * Mathf.Sign(verticalInput);
+        float roll = Mathf.Lerp(0, 30, Mathf.Abs(horizontalInput)) * -Mathf.Sign(horizontalInput);
+        // apply rotation
+        transform.localRotation = Quaternion.Euler(Vector3.up * Yaw + Vector3.right * pitch + Vector3.forward * roll);
+    }
+}
---
+++ Assets/ShootEnemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootEnemy : MonoBehaviour
+{
+    public GameObject nearestEnemy;
+    public GameObject[] Enemies;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Enemies = GameObject.FindGameObjectsWithTag("Enemy");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        foreach (var enemy in Enemies)
+        {
+
+        }
+    }
+}
